17e62301d64f5c1fc7d176168e5abeb7d18d8d2d
janjakg <janjakg@yahoo.fr>
2020-01-15 16:52:21 +0100

    correction camelCase

--- model/postManager.php
+++ model/postManager.php
@@ -1,11 +1,13 @@
 <?php
-require_once('model/Manager.php');
 
+namespace Structure;
+
+require_once('model/Manager.php');
 class PostManager extends Manager
 {
   public function getPosts()
   {
-    $db = $this-> dbconnect();
+    $db = $this-> dbConnect();
     $req = $db-> query('SELECT id, artist, title, DATE_FORMAT(creation_date, \'%d/%m/%Y\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5');
     
     return $req;
