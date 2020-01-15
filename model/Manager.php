6ab38208c2526364f955c9a47c2f6eca770aa454
janjakg <janjakg@yahoo.fr>
2020-01-15 16:53:57 +0100

    activation PDO

--- model/Manager.php
+++ model/Manager.php
@@ -1,9 +1,12 @@
 <?php
+namespace Structure;
+
+use PDO;
 class Manager
 {
     protected function dbConnect()
     {
-      $db = new PDO('mysql:host=localhost;dbname=projet5;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+      $db = new \PDO('mysql:host=localhost;dbname=projet5;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
         return $db;
   }
