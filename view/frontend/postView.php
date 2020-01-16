e669406a17529c31e29cc29fbad122f2650b6233
janjakg <janjakg@yahoo.fr>
2020-01-16 12:59:54 +0100

    ajout du formulaire de commentaires

--- view/frontend/postView.php
+++ view/frontend/postView.php
@@ -19,8 +19,25 @@
     <div id="date">
       <em>le <?= $post['creation_date_fr'] ?></em>
     </div>
-  </div>
+  
   <a href="index.php">retour à l'accueil</a>
+  </div>
+</section>
+<section class="shadow-lg p-3 mb-5 bg-white rounded">
+  <h2>Commentaires</h2>
+  <form action="index.php?action=addComment&amp;id=<?= $post['id'] ?>" method="post">
+    <div class="form-group">
+      <label for="author">Auteur</label>
+      <input type="text" class="form-control" id="author" name="author" />
+    </div>
+    <div class="form-group">
+      <label for="comment">Commentaire</label>
+      <textarea class="form-control" id="comment" name="comment"></textarea>
+    </div>
+    <div>
+      <button type="submit" class="btn btn-primary">Envoi</button>
+    </div>
+  </form>
 </section>
 
 
