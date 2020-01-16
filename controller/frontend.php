09b0c39265bbdb3b3a8e7c0103811d89b90485c8
janjakg <janjakg@yahoo.fr>
2020-01-16 18:21:10 +0100

    complément sur le controller

--- controller/frontend.php
+++ controller/frontend.php
@@ -1,17 +1,19 @@
 <?php
 require 'vendor/autoload.php';
 
-use Structure\{PostManager, Manager};
+use Structure\{PostManager, Manager, CommentManager};
 
 function listPosts()
 {
-    $postManager = new \Structure\PostManager();
+    $postManager = new \Structure\PostManager();    
     $posts = $postManager->getPosts();
     require('view/frontend/listPostsView.php');
 }
 function post()
 {
     $postManager = new \Structure\PostManager();
+    $commentManager = new \Structure\CommentManager();
     $post = $postManager->getPost($_GET['id']);
+    $comments = $commentManager->getComments($_GET['id']);
     require('view/frontend/postView.php');
 }
