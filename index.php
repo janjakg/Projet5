795c6609ce35879e61f7ae920717ad90f77f26b6
janjakg <janjakg@yahoo.fr>
2020-01-17 18:47:33 +0100

    ajout case 'addComment' dans le routeur

--- index.php
+++ index.php
@@ -17,7 +17,20 @@ try
               } else {
                   echo 'Erreur : aucun identifiant de billet envoyé';
               }
-              break;   
+              break; 
+              
+              case 'addComment':
+                if (isset($_GET['id']) && $_GET['id'] > 0) {
+                    if (!empty($_POST['author']) && !empty($_POST['comment'])) {
+                        addComment($_GET['id'], $_POST['author'], $_POST['comment']);
+                    } else {
+                        echo 'Erreur : tous les champs ne sont pas remplis !';
+                    }
+                } else {
+                    echo 'Erreur : aucun identifiant de billet envoyé';
+                }
+                break;
+             
 
             default:
             echo 'Pas d\'action';
