c430374ae0c95f5a722f7cac8bd2b167cbe953ed
janjakg <janjakg@yahoo.fr>
2020-01-21 10:29:19 +0100

    ajout methode getSignalledComment

--- model/commentManager.php
+++ model/commentManager.php
@@ -29,4 +29,10 @@ class CommentManager extends Manager
 
       return $updateComment;
   }
+  public function getSignaledComments()
+  {
+      $db = $this->dbConnect();
+      $signaledComments = $db->query('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr, signalled FROM comments WHERE signalled = 1 ORDER BY comment_date DESC');
+      return $signaledComments;
+  }
 }
