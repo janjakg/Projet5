41beaca9b6f02cbf897ec71b871ad219b480dbff
janjakg <janjakg@yahoo.fr>
2020-01-21 16:08:19 +0100

    footer corrigé

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -11,8 +11,8 @@
   <link rel="stylesheet" href="public/css/style.css">
 </head>
 
-<body> 
-  <div class="bloc_page">
+<body>
+  <nav class="bloc_page">
     <nav class="navbar sticky-top navbar navbar-expand-lg navbar navbar-dark bg-dark">
 
       <a class="navbar-brand" href="#">Nom du Site</a>
@@ -26,7 +26,7 @@
             <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Inscription</a>
+            <a class="nav-link" href="index.php?action=adminRegistration">Inscription</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Connexion</a>
@@ -39,23 +39,25 @@
       <div class="page">
         <?= $content ?>
       </div>
+    </div>
 
-    </div> 
- <div class="footer">
-      <h2>Contact</h2>
-      <p>Ceci est un site étudiant réalisé dans le cadre d'un projet</p>
-    </footer>
-    </div> 
+    <nav class="navbar fixed-bottom navbar-light navbar-dark bg-dark">
+      <div class="container">
+        <h2 class="navbar-text">Contact</h2>
+        <p class="navbar-text">Ceci est un site étudiant réalisé dans le cadre d'un projet</p>
+      </div>    
+    </nav>
+
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-    integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
-  </script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
-  </script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-    integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
-  </script>
- 
+      integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+      integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+      integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    </script>
+
 </body>
 
 </html>
