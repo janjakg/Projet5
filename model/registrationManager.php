7e7247026b93e57baeb01054711c83e43fc07905
janjakg <janjakg@yahoo.fr>
2020-01-21 10:28:55 +0100

    création class regisrationManager

---
+++ model/registrationManager.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Structure;
+
+require 'vendor/autoload.php';
+
+class RegistrationManager extends Manager 
+{
+  public function register($pseudo, $email, $email2, $password, $password2)
+  {
+    $db = $this->dbconnect();  
+    $password = password_hash($_POST['password'],PASSWORD_DEFAULT);   
+    $req = $db->prepare("INSERT INTO member(pseudo, email, password) VALUES('$pseudo', '$email', '$password')");
+    $registration = $req->execute([$pseudo, $email, $email2, $password, $password2]);
+      
+    return $registration;
+  }
+}
+
