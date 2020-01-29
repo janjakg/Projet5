973515709f34d7ab347c8e066140a8761367ff1f
janjakg <janjakg@yahoo.fr>
2020-01-29 10:26:32 +0100

    page indiquant post modifié

---
+++ view/backend/postUpdated.php
@@ -0,0 +1,15 @@
+<?php $title = "Post modifié" ?>
+
+<?php ob_start(); ?>
+<div class="shadow-none m-5 pb-5 bg-light">
+  <h1>Nom du Site</h1>
+</div>
+
+<section class="shadow-lg p-3 mb-5 bg-white rounded">
+  <p>Ce titre a bien été mis à jour. Merci</p>
+  <a href="index.php?action=adminCrud">Retour à la liste des titres</a>
+</section>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('adminTemplate.php'); ?>
