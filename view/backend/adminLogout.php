4ed339a93747de4a74d393eb0dc2a5f710b0db14
janjakg <janjakg@yahoo.fr>
2020-02-19 16:10:39 +0100

    création admin logout

---
+++ view/backend/adminLogout.php
@@ -0,0 +1,18 @@
+<?php $title = "Déconnexion" ?>
+
+<?php ob_start(); ?>
+<div class="shadow-none m-5 pb-5 bg-light">
+  <h1>Nom du site</h1>
+</div>
+
+<section class="shadow-lg p-3 mb-5 bg-white rounded">
+  <p>Vous êtes maintenat déconnecté !</p>
+  <a href="index.php?action=adminLogin">CONNEXION</a>
+</section>
+<section class="shadow-lg p-3 mb-5 bg-white rounded">
+  <a href="index.php?action=listPosts">HOMEPAGE</a>
+</section>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('adminTemplate.php'); ?>
