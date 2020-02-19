38b5c9cb7b0048c4d03471866159e44237327d7b
janjakg <janjakg@yahoo.fr>
2020-02-19 16:08:08 +0100

    ajout du player deezer

--- view/frontend/postView.php
+++ view/frontend/postView.php
@@ -9,28 +9,52 @@
 </div>
 <!--Liste des posts -->
 <section class="news">
-    
 
-  <div class="shadow-lg p-3 mb-5 bg-white rounded" >
-  
-  <!-- 1. The <iframe> (and video player) will replace this <div> tag. -->
-  <div id="player">
-  
-  </div>
+
+  <div class="shadow-lg p-3 mb-5 bg-white rounded">
+    
+      <section id="player">
+        <div id="dz-root"></div>
+        <div id="player" style="width:100%;" align-text="center"></div>
+        <br />
+        <div id="controlers">
+
+          <input type="button" onclick="DZ.player.playAlbum(<?= strip_tags(stripslashes($post['albumName'])) ?>); return false;"
+            value="activer le player" />
+         
+          <br />        
+
+          <br />
+          <input type="button" onclick="DZ.player.play(); return false;" value="Play" />
+          <input type="button" onclick="DZ.player.pause(); return false;" value="Pause" />
+          <input type="button" onclick="DZ.player.prev(); return false;" value="Prev" />
+          <input type="button" onclick="DZ.player.next(); return false;" value="Next" />
+          <br />
+          <input type="button" onclick="DZ.player.setMute(); return false;" value="Set Mute" />
+          <input type="button" onclick="DZ.player.setVolume(20); return false;" value="Set Volume 20" />
+          <input type="button" onclick="DZ.player.setVolume(80); return false;" value="Set Volume 80" />
+
+          <br /><br /><br />
+        </div>
+        <div id="slider_seek" class="progressbarplay" style="">
+          <div class="bar" style="width: 0%;"></div>
+        </div>    
+      </section>  
 
     <h2>"<?= strip_tags(stripslashes($post['title'])) ?>"</h2>
-    <p>Titre de : 
-      <?= strip_tags(stripslashes($post['artist'])) ?>
+    <p>Est mon titre préféré de l'album de :
+      <em><?= strip_tags(stripslashes($post['artist'])) ?></em>
     </p>
+    <p>Vous pouvez indiquer le votre en commentaire</p>
     <div id="date">
       <em>le <?= $post['creation_date_fr'] ?></em>
     </div>
-  
-  <a href="index.php">retour à l'accueil</a>
+
+    <a href="index.php">retour à l'accueil</a>
   </div>
 </section>
 <section class="shadow-lg p-3 mb-5 bg-white rounded">
-  <h2>Commentaires</h2> 
+  <h2>Commentaires</h2>
   <form action="index.php?action=addComment&amp;id=<?= $post['id'] ?>" method="post">
     <div class="form-group">
       <label for="author">Auteur</label>
