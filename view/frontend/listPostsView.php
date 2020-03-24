ab308965af8a40985956689b8e39ce89c83dff76
janjakg <janjakg@yahoo.fr>
2020-03-24 14:40:41 +0100

    correction listPostView

--- view/frontend/listPostsView.php
+++ view/frontend/listPostsView.php
@@ -7,35 +7,34 @@ $title = "Decouvertes";
 
 <div class="banner">
 
-<div class="jumbotron ">
-  <div class="container">
-    <h1 class="display-4 ">Tracklist...</h1>    
-  </div>
-</div>
-
-<div class="row row-cols-1 row-cols-md-3">
-
-  <?php while ($data = $posts->fetch()):?>
-
-  <div class="col mb-5 pb-5">
-    <div class="card">
-      <img src="<?= strip_tags(stripslashes($data['imageAlbum'])) ?>",
- class="card-img-top" alt="album_cover">
-      <div class="card-body">
-        <h5 class="card-title"><?= strip_tags(stripslashes($data['artist'])) ?></h5>
-        <p class="card-text"><?= nl2br(strip_tags(stripslashes(substr($data['title'],0,200)))) ?></p>
-        <a href="index.php?action=post&amp;id=<?= $data['id'] ?>" class="btn btn-primary track">voir plus</a>
-      </div>
+  <div class="jumbotron ">
+    <div class="container">
+      <h1 class="display-4 ">Tracklist...</h1>
     </div>
   </div>
 
-  <?php endwhile;?>
+  <div class="row row-cols-1 row-cols-md-3">
 
-</div>
+    <?php while ($data = $posts->fetch()):?>
+
+    <div class="col mb-5 pb-5">
+      <div class="card">
+        <img src="<?= strip_tags(stripslashes($data['imageAlbum'])) ?>" , class="card-img-top" alt="album_cover">
+        <div class="card-body">
+          <h5 class="card-title"><?= strip_tags(stripslashes($data['artist'])) ?></h5>
+          <p class="card-text"><?= nl2br(strip_tags(stripslashes(substr($data['title'],0,200)))) ?></p>
+          <a href="index.php?action=post&amp;id=<?= $data['id'] ?>" class="btn btn-primary track">voir plus</a>
+        </div>
+      </div>
+    </div>
+
+    <?php endwhile;?>
+
+  </div>
 
 
 
 
-<?php $content = ob_get_clean(); ?>
+  <?php $content = ob_get_clean(); ?>
 
-<?php require('template.php'); ?>
+  <?php require('template.php'); ?>
