fc10f706ba3773b21cc74cd34700063cfd2abe2e
janjakg <janjakg@yahoo.fr>
2020-03-30 21:15:39 +0200

    refacto adminIndex.php

--- view/backend/adminIndex.php
+++ view/backend/adminIndex.php
@@ -8,53 +8,27 @@
   </div>
 </div>
 
-<div id="accordion">
-  <div class="card">
-    <div class="card-header" id="headingOne">
-      <h5 class="mb-0">
-        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        Gérer les commentaires signalés
-        </button>
-      </h5>
-    </div>
-    <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion"> 
-      <div class="table-responsive-md">
-        <table class="table table-hover table-bordered">
-          <thead>
-            <tr>
-              <th scope="col">Comment</th>
-              <th colspan="2" scope="col">Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            <?php while ($row = $signaledComments->fetch()):?>
-            <tr>
-              <td><?= nl2br(htmlspecialchars($row['comment'])) ?></td>
-              <td><a class="btn btn-danger"
-                  href="index.php?action=eraseComment&amp;idComment=<?= ($row['id']) ?>">Supprimer</a></td>
-              <?php if ($row['signalled'] == 1): ?>
-              <td><a href="index.php?action=saveComment&amp;commentId=<?= ($row['id']) ?>"
-                  class="btn btn-success">Valider</a></td>
-              <?php endif; ?>
-            </tr>
-            <?php endwhile; ?>
-          </tbody>
-        </table>
-      </div>    
+<div class="row">
+  <div class="col-sm-6 mb-5">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title"> Gérer les commentaires signalés</h5>
+        <p class="card-text">Cet espace permettra de modérer les commentaires.</p>
+        <a href="index.php?action=adminIndexComment" class="btn btn-primary">Go somewhere</a>
+      </div>
     </div>
   </div>
-
-  <div class="card">
-    <div class="card-header" id="headingTwo">
-      <h5 class="mb-0">
-        <a href="index.php?action=adminCrud">
-        Gestion du contenu : création, mise à jour et suppression
-        </a>
-      </h5>
-    </div>    
-  </div>
-
   
+  <div class="col-sm-6 mb-5">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">  Gestion du contenu :</h5>
+        <p class="card-text"> Espace pour création, mise à jour et suppression de titres.</p>
+        <a href="index.php?action=adminCrud" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+</div> 
 
 <?php $content = ob_get_clean(); ?>
 
