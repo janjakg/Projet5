a2a75704a11934c55a619576ea8c56a3ca61ea97
janjakg <janjakg@yahoo.fr>
2020-03-30 21:09:37 +0200

    correction du footer

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -12,9 +12,9 @@
 </head>
 
 <body>
-  <nav class="bloc_page">
+  <div class="bloc_page">
     <nav class="navbar sticky-top navbar  navbar navbar-dark bg-dark">
-    <?php if (isset($_SESSION['pseudo'])) :?>
+      <?php if (isset($_SESSION['pseudo'])) :?>
       <span class="badge badge-pill badge-success">connecté </span>
       <?php else :?>
       <p> </p>
@@ -30,14 +30,14 @@
             <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
           </li>
           <?php if (isset($_SESSION['pseudo'])) :?>
-            <li class="nav-item">
+          <li class="nav-item">
             <a class="nav-link" href="index.php?action=adminLogin">Dashboard</a>
-          </li> 
+          </li>
           <li class="nav-item">
             <a class="nav-link" href="index.php?action=adminLogout">déconnexion</a>
-          </li>        
+          </li>
           <?php else :?>
-            <li class="nav-item">
+          <li class="nav-item">
             <a class="nav-link" href="index.php?action=adminRegistration">Inscription</a>
           </li>
           <li class="nav-item">
@@ -49,32 +49,34 @@
     </nav>
 
     <div class="container">
-      <div class="page">
-        <?= $content ?>
-      </div>
+    <div class="page">
+      <?= $content ?>
     </div>
+  </div>
+  </div>
 
-    <nav class="navbar fixed-bottom navbar-light navbar-dark bg-dark">
-      <div class="container">
-        <h2 class="navbar-text">Contact</h2>
-        <p class="navbar-text">Ceci est un site étudiant réalisé dans le cadre d'un projet</p>
-      </div>    
-    </nav>
 
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-      integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-      integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
-    </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-      integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
-    </script>
-<div id="dz-root"></div>
-<script src="https://e-cdns-files.dzcdn.net/js/min/dz.js"></script>
+  <nav class="navbar sticky-bottom navbar-light navbar-dark bg-dark">
+    <div class="container">
+      <h2 class="navbar-text">Contact</h2>
+      <p class="navbar-text">Ceci est un site étudiant réalisé dans le cadre d'un projet</p>
+    </div>
+  </nav>
 
-    <script src="public/js/track.js"></script>
-    <script src="public/js/player.js"></script>
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+    integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+  </script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+  </script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+    integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+  </script>
+  <div id="dz-root"></div>
+  <script src="https://e-cdns-files.dzcdn.net/js/min/dz.js"></script>
+
+  <script src="public/js/track.js"></script>
+  <script src="public/js/player.js"></script>
 
 </body>
 
