e738452b68826a01708753c730a7998b2c6199b6
janjakg <janjakg@yahoo.fr>
2020-03-30 21:13:53 +0200

    correction footer adminTemplate

--- view/backend/adminTemplate.php
+++ view/backend/adminTemplate.php
@@ -13,7 +13,7 @@
 </head>
 
 <body>
-  <nav class="bloc_page">
+  <div class="bloc_page">
     <nav class="navbar sticky-top navbar  navbar-dark bg-dark">
       <?php if (isset($_SESSION['pseudo'])) :?>
       <span class="badge badge-pill badge-success">connecté </span>
@@ -56,27 +56,44 @@
       </div>
     </div>
 
-    <nav class="navbar fixed-bottom navbar-light navbar-dark bg-dark">
+   
+    <nav class="navbar sticky-bottom navbar-light navbar-dark bg-dark">
       <div class="container">
         <h2 class="navbar-text">Contact</h2>
         <p class="navbar-text">Ceci est un site étudiant réalisé dans le cadre d'un projet</p>
       </div>
-    </nav>
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-      integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
-    </script>
-    <script src="https://code.jquery.com/jquery-3.4.1.js"
-      integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-      integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
-    </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-      integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
-    </script>
-    <div id="dz-root"></div>
-    <script src="https://e-cdns-files.dzcdn.net/js/min/dz.js"></script>
-    <script src="public/js/track.js"></script>
-    <script src="public/js/player.js"></script>
+    </nav>   
+    
+  
+
+  
+   <!--
+  <footer>
+    <div class="container fixed-bottom">
+      Site réalisé par Jean-Jacques GOMIS dans le cadre de la formation OpenClassrooms. 2019
+    </div>
+  </footer>
+  </div>
+     -->
+
+
+
+
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+    integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+  </script>
+  <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+  </script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+    integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+  </script>
+  <div id="dz-root"></div>
+  <script src="https://e-cdns-files.dzcdn.net/js/min/dz.js"></script>
+  <script src="public/js/track.js"></script>
+  <script src="public/js/player.js"></script>
 
 
 </body>
