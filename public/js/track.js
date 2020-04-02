bba26a1a8e551b79f01c73473dca6f0aa850587b
janjakg <janjakg@yahoo.fr>
2020-04-02 12:32:24 +0200

    suppression commentaire inutile

--- public/js/track.js
+++ public/js/track.js
@@ -1,47 +1,3 @@
-/*$(document).ready(function() {
-    const apiUrl = 'https://www.googleapis.com/youtube/v3/playlists';
-    const format = '&format = json';
-    var key = 'AIzaSyDS4Qxs1LHcgXerd38MVTSeIyAlZCRxbPo';
-    var playlistId = 'zJJEtBempG0';
-
-    let artist = $('#artist');
-    let title = $('#title');
-    let album = $('#album');
-    let image = $('#image');
-    let errorMessage = $('#error-message');
-
-
-
-    $(artist).on('blur', function() {
-        let singer = $(this).val();
-        //console.log(singer);
-        let url = apiUrl + singer + format;
-        //console.log(url);
-
-        fetch(url, { method: 'get' }).then(response => response.json()).then(results => {
-            console.log(results);
-            if (results) {
-                $.each(results, function(key, value) {
-                    //console.log(value);
-                    console.log(key, value);
-                    $(title).append('<option value="' + key + '">' + value + '</option>');
-                    $(album).append('<option value="' + key + '">' + value + '</option>');
-                    $(image).append('<option value="' + key + '">' + value + '</option>');
-                });
-            } else {
-                if ($(artist).val()) {
-                    console.log('artiste inexistant');
-                    $(errorMessage).text('artiste inexistant dans notre base').show();
-                } else {
-                    $(errorMessage).text('').hide();
-                }
-            }
-        }).catch(err => {
-            console.log(err);
-        });
-    });
-});*/
-
 var callBackGetSucess = function(data) {
     console.log("données api", data);
     //alert("info titre : " + data['data'][1]['artist'].id);
@@ -50,11 +6,11 @@ var callBackGetSucess = function(data) {
     var element = document.getElementById("zoneArtist");
     //element.innerHTML = data['data'][0]['artist'].name + " (id numéro : " + data['data'][0]['artist'].id + ") , est l'auteur du titre '" + data['data'][0].title + " ' issu de l'album : " + data['data'][0]['album'].title + " et la pochette de son album est visible à l'url : " + data['data'][0]['album'].cover_medium;
     //document.getElementById("infoToPaste").innerHTML = "à l'aide de ces données remplissez le formulaire avant envoi"
-    document.getElementById("artistName").innerHTML = data['data'][0]['artist'].name
-    document.getElementById("artistId").innerHTML = data['data'][0]['artist'].id
-    document.getElementById("artistTrack").innerHTML = data['data'][0].title
-    document.getElementById("albumName").innerHTML = data['data'][0]['album'].id
-    document.getElementById("imageAlbum").innerHTML = data['data'][0]['album'].cover_medium
+    document.getElementById("artistName").innerHTML = data['data'][0]['artist'].name + '  '
+    document.getElementById("artistId").innerHTML = data['data'][0]['artist'].id + '  '
+    document.getElementById("artistTrack").innerHTML = data['data'][0].title + '  '
+    document.getElementById("albumName").innerHTML = data['data'][0]['album'].id + '  '
+    document.getElementById("imageAlbum").innerHTML = data['data'][0]['album'].cover_medium + '  '
 }
 
 function buttonClickGet() {
@@ -76,3 +32,36 @@ function buttonClickGet() {
         });
 
 }
+
+/*(() => {
+    const button = document.querySelector('#btnCopy');
+
+    button.addEventListener('click', () => {
+        const text = document.querySelector('#artistName');
+        const range = document.createRange();
+
+        range.selectNode(text);
+        window.getSelection().addRange(range);
+
+        try {
+            if (document.execCommand('copy')) {
+                alert('texte copié')
+            }
+        } catch (error) {
+            alert('copie impossible');
+        }
+        //  window.getSelection().removeAllRanges();
+    });
+
+})();*/
+
+function copyToClip(str) {
+    function listener(e) {
+        e.clipboardData.setData("text/html", str);
+        e.clipboardData.setData("text/plain", str);
+        e.preventDefault();
+    }
+    document.addEventListener("copy", listener);
+    document.execCommand("copy");
+    document.removeEventListener("copy", listener);
+};
