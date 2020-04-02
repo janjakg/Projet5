5cc7c996fa10880209701c7b6911b1054d099825
janjakg <janjakg@yahoo.fr>
2020-04-02 12:30:35 +0200

    ajout google font

--- view/backend/adminTemplate.php
+++ view/backend/adminTemplate.php
@@ -7,12 +7,14 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
     integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-  <link href="https://fonts.googleapis.com/css?family=Shrikhand&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/fa6a9c271b.js" crossorigin="anonymous"></script>
+      <link href="https://fonts.googleapis.com/css?family=Shrikhand&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@700&family=Roboto+Condensed:wght@300&family=Slabo+27px&display=swap" rel="stylesheet">
   <title>projet5</title>
   <link rel="stylesheet" href="public/css/style.css">
 </head>
 
-<body>
+<body class="adminBody">
   <div class="bloc_page">
     <nav class="navbar sticky-top navbar  navbar-dark bg-dark">
       <?php if (isset($_SESSION['pseudo'])) :?>
