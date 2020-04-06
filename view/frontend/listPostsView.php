5954d09819ae730a97b0a7a71645035cc6398c4c
janjakg <janjakg@yahoo.fr>
2020-04-06 22:57:43 +0200

    finition pagination

--- view/frontend/listPostsView.php
+++ view/frontend/listPostsView.php
@@ -4,7 +4,6 @@ $title = "Decouvertes";
 ?>
 
 <?php ob_start();?>
-
 <div class="banner">
 
   <div class="jumbotron ">
@@ -23,7 +22,8 @@ $title = "Decouvertes";
         <div class="card-body">
           <h5 class="card-title"><?= strip_tags(stripslashes($data['artist'])) ?></h5>
           <p class="card-text"><?= nl2br(strip_tags(stripslashes(substr($data['title'],0,200)))) ?></p>
-          <a href="index.php?action=post&amp;id=<?= $data['id'] ?>" class="btn btn-primary track">voir plus</a>
+          <a href="index.php?action=post&amp;id=<?= $data['id'] ?>" class="btn btn-primary track"><i
+              class="fas fa-play-circle"></i> Play</a>
         </div>
       </div>
     </div>
@@ -32,11 +32,41 @@ $title = "Decouvertes";
 
   </div>
 
+  <?php $page = (!empty($_GET['page']) ? $_GET['page'] : 1);
+    $lastPage = 5; 
+  ?>
 
-  <?php $page = (!empty($_GET['page']) ? $_GET['page'] : 1);?>
+  <nav aria-label="Page navigation example">
+    <ul class="pagination justify-content-center">
 
-  <a href="?page=<?php echo $page - 1; ?>">Page précédente</a>
-  <a href="?page=<?php echo $page + 1; ?>">Page suivante</a>
+      <?php
+
+/* Si on est sur la première page, on n'a pas besoin d'afficher de lien
+ * vers la précédente. On va donc l'afficher que si on est sur une autre
+ * page que la première */
+
+if ($page > 1):
+    ?><li class="page-item"><a class="page-link" href="?page=<?php echo $page - 1; ?>">Previous</a></li> <?php
+endif;
+
+/* On va effectuer une boucle autant de fois que l'on a de pages */
+for ($i = 1; $i <= $lastPage; $i++):
+    ?><li class="page-item"><a class="page-link" href="?page=<?php echo $i; ?>"><?php echo $i; ?></a></li> <?php
+endfor;
+
+/* Avec le nombre total de pages, on peut aussi masquer le lien
+ * vers la page suivante quand on est sur la dernière */
+if ($page < $lastPage):
+    ?><li class="page-item"> <a class="page-link" href="?page=<?php echo $page + 1; ?>">Next</a></li><?php
+endif;
+?>
+    </ul>
+  </nav>
+
+
+
+
+
 
   <?php $content = ob_get_clean(); ?>
 
