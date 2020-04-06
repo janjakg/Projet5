75bf4e47d2ff3b8abc7ba785a79cc5b09a2c6fb5
janjakg <janjakg@yahoo.fr>
2020-04-06 22:58:37 +0200

    mise à jour

--- view/backend/erasePost.php
+++ view/backend/erasePost.php
@@ -1,8 +1,8 @@
 <?php $title = "Post supprimé" ?>
 
 <?php ob_start(); ?>
-<div class="shadow-none m-5 pb-5 bg-light">
-  <h1>Nom du Site</h1>
+<div class="shadow-none m-5 pb-5">
+  <h1 class="text-center">Suppression</h1>
 </div>
 
 <section class="shadow-lg p-3 mb-5 bg-white rounded">
