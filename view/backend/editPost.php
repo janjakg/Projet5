711b05775ab025448df677b9c2510b53303087cf
janjakg <janjakg@yahoo.fr>
2020-04-06 22:59:12 +0200

    ajout de style

--- view/backend/editPost.php
+++ view/backend/editPost.php
@@ -2,8 +2,8 @@
 
 <?php ob_start(); ?>
 
-<div class="shadow-none m-5 pb-5 bg-light">
-  <h1 class="text-center">Nom du Site</h1>
+<div class="shadow-none m-5 pb-5 bg">
+  <h1 class="text-center">Mise à Jour</h1>
 </div>
 
 <section class="shadow-lg p-3 mb-5 bg-white rounded">
@@ -14,17 +14,15 @@
     <div class="form-group">
       <label for="artist">Artiste</label>
       <input type="text" class="form-control" name="artist" id="artist"
-        value="<?= stripslashes(strip_tags($post['artist'])) ?>">
+        value="<?= stripslashes(strip_tags($post['artist'])) ?>: <?=  stripslashes(strip_tags($post['title']))  ?>" >
     </div>
     <div class="form-group">
-    <label for="title">Contenu</label>
-    <textarea class="form-control" id="title" rows="3"><?= stripslashes(strip_tags($post['title']))  ?></textarea>
+    <label for="title">Mise à jour :</label>
+    <textarea class="form-control" id="title" rows="3"> Si ce titre ne vous convient pas supprimez le et selectionnez en un autre.</textarea>
   </div>
-    <button type="submit" class="btn btn-primary">Modifier</button>
+    
   </form>
 </section>
-
-
 
 <?php $content = ob_get_clean(); ?>
 
