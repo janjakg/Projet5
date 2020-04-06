dcf20bb4748a6929894332b958433d7bbb22a019
janjakg <janjakg@yahoo.fr>
2020-04-06 22:58:20 +0200

    réindentation

--- view/backend/readPost.php
+++ view/backend/readPost.php
@@ -2,22 +2,17 @@
 
 <?php ob_start(); ?>
 <div class="shadow-none m-5 pb-5 bg">
-  <h1 class="text-center"></h1>
+  <h1 class="text-center">Lecture</h1>
 </div>
-
 
 <section class="shadow-lg p-3 mb-5 bg-white rounded" align-text="center">
 
 <a href="index.php?action=adminCrud">Retour à la liste des titres</a>
 
-
   <div id="trackFont">" <?= strip_tags(stripslashes(($displayPost['title'])))?> "</div>
   <p id="artistPosition">de:
     <strong><?= strip_tags(stripslashes(strtoupper($displayPost['artist']))) ?></strong>
   </p>
-
-
-
   <div class="card mb-3" style="max-width: 540px;">
     <div class="row no-gutters">
       <div class="col-md-4">
@@ -27,15 +22,11 @@
         <div class="card-body">
           <h5 class="card-title"><?= strip_tags(stripslashes($displayPost['artist'])) ?></h5>
           <p class="card-text"><?= nl2br(strip_tags(stripslashes(substr($displayPost['title'],0,200)))) ?></p>
-
         </div>
       </div>
     </div>
   </div>
-
 </section>
-
-
 
 <section class="shadow-lg p-3 mb-5 bg-white rounded" id="player">
   <div id="dz-root"></div>
@@ -51,8 +42,6 @@
 
   </div>
 </section>
-
-
 
 <?php $content = ob_get_clean(); ?>
 
