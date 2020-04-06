addeb7a985e2c0016b266084ea331820dbe6a4ef
janjakg <janjakg@yahoo.fr>
2020-04-06 23:00:05 +0200

    ajout de police

--- view/backend/adminTemplate.php
+++ view/backend/adminTemplate.php
@@ -7,9 +7,12 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
     integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <script src="https://kit.fontawesome.com/fa6a9c271b.js" crossorigin="anonymous"></script>
-      <link href="https://fonts.googleapis.com/css?family=Shrikhand&display=swap" rel="stylesheet">
-      <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@700&family=Roboto+Condensed:wght@300&family=Slabo+27px&display=swap" rel="stylesheet">
+  <script src="https://kit.fontawesome.com/fa6a9c271b.js" crossorigin="anonymous"></script>
+  <link href="https://fonts.googleapis.com/css?family=Shrikhand&display=swap" rel="stylesheet">
+  <link
+    href="https://fonts.googleapis.com/css2?family=Cinzel:wght@700&family=Roboto+Condensed:wght@300&family=Slabo+27px&display=swap"
+    rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
   <title>projet5</title>
   <link rel="stylesheet" href="public/css/style.css">
 </head>
@@ -58,45 +61,29 @@
       </div>
     </div>
 
-   
+
     <nav class="navbar sticky-bottom navbar-light navbar-dark bg-dark">
       <div class="container">
         <h2 class="navbar-text">Contact</h2>
         <p class="navbar-text">Ceci est un site étudiant réalisé dans le cadre d'un projet</p>
       </div>
-    </nav>   
-    
-  
+    </nav>
 
-  
-   <!--
-  <footer>
-    <div class="container fixed-bottom">
-      Site réalisé par Jean-Jacques GOMIS dans le cadre de la formation OpenClassrooms. 2019
-    </div>
-  </footer>
-  </div>
-     -->
-
-
-
-
-  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-    integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
-  </script>
-  <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
-    crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
-  </script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-    integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
-  </script>
-  <div id="dz-root"></div>
-  <script src="https://e-cdns-files.dzcdn.net/js/min/dz.js"></script>
-  <script src="public/js/track.js"></script>
-  <script src="public/js/player.js"></script>
-
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+      integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    </script>
+    <script src="https://code.jquery.com/jquery-3.4.1.js"
+      integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+      integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+      integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    </script>
+    <div id="dz-root"></div>
+    <script src="https://e-cdns-files.dzcdn.net/js/min/dz.js"></script>
+    <script src="public/js/track.js"></script>
+    <script src="public/js/player.js"></script>
 
 </body>
 
