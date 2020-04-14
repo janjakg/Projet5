7b1fa41918b8c4e3bb7561003ad9dec231d531f4
janjakg <janjakg@yahoo.fr>
2020-04-14 16:33:27 +0200

    correction d'erreurs

--- model/Manager.php
+++ model/Manager.php
@@ -1,16 +1,5 @@
 <?php
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> pagination2
-=======
-
->>>>>>> pagination2
->>>>>>> master
 namespace Structure;
 
 use PDO;
@@ -18,7 +7,7 @@ class Manager
 {
     protected function dbConnect()
     {
-      $db = new \PDO('mysql:host=localhost;dbname=projet5;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+      $db = new PDO('mysql:host=localhost;dbname=projet5;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
         return $db;
   }
