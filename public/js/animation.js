817ebe626aed2c2c4bf364fea3ef27043c13c477
janjakg <janjakg@yahoo.fr>
2020-04-14 23:02:55 +0200

    js corrigé

--- public/js/animation.js
+++ public/js/animation.js
@@ -1,8 +1,4 @@
 //Petite animation de texte sur la page d'accueil
-<<<<<<< HEAD
-
-=======
->>>>>>> 4723d87a693a405cd4a12f3d5bca841af09bd651
 const txtAnim = document.querySelector('h1');
 new Typewriter(txtAnim, {
         //deleteSpeed: 20
@@ -12,9 +8,5 @@ new Typewriter(txtAnim, {
     .typeString('<span style="font-family:Homemade Apple">For Music Lover</span>')
     .pause(1000)
     .deleteChars(15)
-<<<<<<< HEAD
-    .typeString('<span style="color:red" style="font-family:Homemade Apple"> For Music Lovers Only...</span>')
-=======
     .typeString('<span style="color:red" style="font-family:Homemade Apple"> For Music Lovers...</span>')
->>>>>>> 4723d87a693a405cd4a12f3d5bca841af09bd651
     .start()
