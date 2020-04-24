215b834ce0eb05b9cbb0f7b3b9f4f9df1b77ecf8
janjakg <janjakg@yahoo.fr>
2020-04-24 18:36:09 +0200

    ré indentation

--- model/loginManager.php
+++ model/loginManager.php
@@ -6,23 +6,23 @@ require 'vendor/autoload.php';
 
 class LoginManager extends Manager
 {
-  public function login($email, $password)
-  {
-    $db = $this->dbConnect();
-    $req = $db->prepare('SELECT * FROM members WHERE email=?');
-    $req->execute([$email]);
-    $member = $req->fetch();
-    $password = password_verify($_POST['password'], $member['password']);
+    public function login($email, $password)
+    {
+        $db = $this->dbConnect();
+        $req = $db->prepare('SELECT * FROM members WHERE email=?');
+        $req->execute([$email]);
+        $member = $req->fetch();
+        $password = password_verify($_POST['password'], $member['password']);
 
-    return $member;
-  }
+        return $member;
+    }
 
-  public function logout()
-  {
-    $db = $this->dbconnect();
-    $req = $db->prepare("UPDATE members SET email = '?', password ='?' WHERE email= '?'");   
-    $disconnect = $req;
+    public function logout()
+    {
+        $db = $this->dbconnect();
+        $req = $db->prepare("UPDATE members SET email = '?', password ='?' WHERE email= '?'");
+        $disconnect = $req;
 
-    return $disconnect;
-  }
+        return $disconnect;
+    }
 }
