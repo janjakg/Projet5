e03bf697634b820c55cfd3ebc9009abace69633b
janjakg <janjakg@yahoo.fr>
2020-04-24 18:27:01 +0200

    ré indentation et correction

--- view/backend/editPost.php
+++ view/backend/editPost.php
@@ -1,6 +1,6 @@
-<?php $title = "Post à modifier" ?>
+<?php $title = "Post à modifier"?>
 
-<?php ob_start(); ?>
+<?php ob_start();?>
 
 <div class="shadow-none m-5 pb-5 bg">
   <h1 class="text-center">Mise à Jour</h1>
@@ -10,20 +10,19 @@
   <h2>Titre à modifier</h2>
   <a href="index.php?action=adminCrud">Retour à la liste des titres</a>
 
-  <form action="index.php?action=updatePost&idPost=<?=$post['id'] ?>" method="post">
+  <form action="index.php?action=updatePost&idPost=<?=$post['id']?>" method="post">
     <div class="form-group">
-      <label for="artist">Artiste</label>
+      <label for="artist">Titre</label>
       <input type="text" class="form-control" name="artist" id="artist"
-        value="<?= stripslashes(strip_tags($post['artist'])) ?>: <?=  stripslashes(strip_tags($post['title']))  ?>" >
+        value="<?=stripslashes(strip_tags($post['artist']))?>: <?=stripslashes(strip_tags($post['title']))?>">
     </div>
-    <div class="form-group">
-    <label for="title">Mise à jour :</label>
-    <textarea class="form-control" id="title" rows="3"> Si ce titre ne vous convient pas supprimez le et selectionnez en un autre.</textarea>
-  </div>
-    
+    <p>Si ce titre ne vous convient pas <a href="index.php?action=adminCrud"><strong>SUPPRIMEZ LE,</strong></a> et
+      n'hesitez pas à en choisir un autre</p>
+    <a href="index.php?action=createPost" class="btn btn-primary">Nouvelle recherche</a>
+
   </form>
 </section>
 
-<?php $content = ob_get_clean(); ?>
+<?php $content = ob_get_clean();?>
 
-<?php require('adminTemplate.php'); ?>
+<?php require 'adminTemplate.php';?>
