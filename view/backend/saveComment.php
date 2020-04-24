6d265cd7c27d82e7a49643fa9971bd852d8be441
janjakg <janjakg@yahoo.fr>
2020-04-24 18:25:16 +0200

    ré-indentation et commentaires

--- view/backend/saveComment.php
+++ view/backend/saveComment.php
@@ -1,13 +1,16 @@
-<?php $title = "Commentaire sauvegardé" ?>
+<?php $title = "Commentaire sauvegardé"?>
 
-<?php ob_start(); ?>
-
-
+<?php ob_start();?>
+<div class="headline">
+  <div class="shadow-none m-5 pb-5 ">
+    <h1 class="text-center">Commentaires sauvegardé</h1>
+  </div>
+</div>
 <section class="shadow-lg p-3 mb-5 bg-white rounded">
   <p>Ce commentaire est sauvegardé</p>
   <a href="index.php?action=adminIndex">Retour aux commentaires signalés</a>
 </section>
 
-<?php $content = ob_get_clean(); ?>
+<?php $content = ob_get_clean();?>
 
-<?php require('adminTemplate.php'); ?>
+<?php require 'adminTemplate.php';?>
