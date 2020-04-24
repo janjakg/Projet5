8c2585b61b08017625f598438842467729dec64b
janjakg <janjakg@yahoo.fr>
2020-04-24 18:24:46 +0200

    réindentation

--- view/backend/updateInfo.php
+++ view/backend/updateInfo.php
@@ -1,6 +1,6 @@
-<?php $title = "Un titre a été créé" ?>
+<?php $title = "Un titre a été créé"?>
 
-<?php ob_start(); ?>
+<?php ob_start();?>
 <div class="headline">
   <div class="shadow-none m-5 pb-5 ">
     <h1 text-align="center">Mise à jour</h1>
@@ -12,11 +12,10 @@
   <p>Un titre a bien été modifié. Merci</p>
   <?php else: ?>
   <p>Un titre a bien été créé. Merci</p>
-  <?php endif; ?>
-
-  <a href="index.php?action=createPost">Retour à l'envoi du titre</a>
+  <?php endif;?>
+  <a href="index.php?action=adminCrud">Retour à la liste des titres</a>
 </section>
 
-<?php $content = ob_get_clean(); ?>
+<?php $content = ob_get_clean();?>
 
-<?php require('adminTemplate.php'); ?>
+<?php require 'adminTemplate.php';?>
