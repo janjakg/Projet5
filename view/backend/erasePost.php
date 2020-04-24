e967cb17286ceb2622aaa196e850e774e1a45504
janjakg <janjakg@yahoo.fr>
2020-04-24 18:26:17 +0200

    réindentation

--- view/backend/erasePost.php
+++ view/backend/erasePost.php
@@ -1,6 +1,6 @@
-<?php $title = "Post supprimé" ?>
+<?php $title = "Post supprimé"?>
 
-<?php ob_start(); ?>
+<?php ob_start();?>
 <div class="shadow-none m-5 pb-5">
   <h1 class="text-center">Suppression</h1>
 </div>
@@ -10,6 +10,6 @@
   <a href="index.php?action=adminCrud">Retour à la liste des posts</a>
 </section>
 
-<?php $content = ob_get_clean(); ?>
+<?php $content = ob_get_clean();?>
 
-<?php require('adminTemplate.php'); ?>
+<?php require 'adminTemplate.php';?>
