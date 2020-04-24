06e72762d79f8374ede34757011768b502577e88
janjakg <janjakg@yahoo.fr>
2020-04-24 18:29:54 +0200

    réindentation et commentaires

--- view/backend/adminIndexComment.php
+++ view/backend/adminIndexComment.php
@@ -1,11 +1,11 @@
-<?php $title = "Post modifié" ?>
-
-<?php ob_start(); ?>
+<?php $title = "Post modifié"?>
+<!--On a ici la liste de tous les commentaires signalés -->
+<?php ob_start();?>
 <div class="shadow-none m-5 pb-5 ">
   <h1 class="text-center">Commentaires signalés</h1>
 </div>
-<a  href="index.php?action=adminIndex" >
-retour au tableau de bord
+<a href="index.php?action=adminIndex">
+  retour au tableau de bord
 </a>
 
 <div class="table-responsive-md">
@@ -17,21 +17,24 @@ retour au tableau de bord
       </tr>
     </thead>
     <tbody>
-      <?php while ($row = $signaledComments->fetch()):?>
+      <!-- Avec cette boucle on va chercher dans la base tous les commentaires signalés -->
+      <?php while ($row = $signaledComments->fetch()): ?>
       <tr>
-        <td><?= nl2br(htmlspecialchars($row['comment'])) ?></td>
-        <td><a class="btn btn-danger"
-            href="index.php?action=eraseComment&amp;idComment=<?= ($row['id']) ?>">Supprimer</a></td>
-        <?php if ($row['signalled'] == 1): ?>
-        <td><a href="index.php?action=saveComment&amp;commentId=<?= ($row['id']) ?>" class="btn btn-success">Valider</a>
+        <td>
+          <?=nl2br(htmlspecialchars($row['comment']))?>
         </td>
-        <?php endif; ?>
+        <td><a class="btn btn-danger" href="index.php?action=eraseComment&amp;idComment=<?=($row['id'])?>">Supprimer</a>
+        </td>
+        <?php if ($row['signalled'] == 1): ?>
+        <td><a href="index.php?action=saveComment&amp;commentId=<?=($row['id'])?>" class="btn btn-success">Valider</a>
+        </td>
+        <?php endif;?>
       </tr>
-      <?php endwhile; ?>
+      <?php endwhile;?>
     </tbody>
   </table>
 </div>
 
-<?php $content = ob_get_clean(); ?>
+<?php $content = ob_get_clean();?>
 
-<?php require('adminTemplate.php'); ?>
+<?php require 'adminTemplate.php';?>
