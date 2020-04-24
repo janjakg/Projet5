ad71da1a50b42e90ea3e512482c66a3e6bb82c3f
janjakg <janjakg@yahoo.fr>
2020-04-24 18:26:37 +0200

    réindentation

--- view/backend/eraseComment.php
+++ view/backend/eraseComment.php
@@ -1,12 +1,18 @@
-<?php $title = "Commentaire supprimé" ?>
+<?php $title = "Commentaire supprimé"?>
 
-<?php ob_start(); ?>
+<div class="headline">
+  <div class="shadow-none m-5 pb-5 ">
+    <h1 class="text-center">Commentaires supprimé</h1>
+  </div>
+</div>
+
+<?php ob_start();?>
 
 <section class="shadow-lg p-3 mb-5 bg-white rounded">
   <p>Ce commentaire a bien été supprimé. Merci</p>
   <a href="index.php?action=adminIndex">Retour aux commentaires signalés</a>
 </section>
 
-<?php $content = ob_get_clean(); ?>
+<?php $content = ob_get_clean();?>
 
-<?php require('adminTemplate.php'); ?>
+<?php require 'adminTemplate.php';?>
