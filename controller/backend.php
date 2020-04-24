c56f84af7c899197b1719b1b861e508a0431851a
janjakg <janjakg@yahoo.fr>
2020-04-24 18:37:43 +0200

    ré indentation

--- controller/backend.php
+++ controller/backend.php
@@ -2,86 +2,92 @@
 session_start();
 require 'vendor/autoload.php';
 
-use Structure\{PostManager, Manager, CommentManager, RegistrationManager, LoginManager};
+use Structure\CommentManager;
+use Structure\LoginManager;
+use Structure\PostManager;
+use Structure\RegistrationManager;
 
 function adminRegistration()
-{      
-    require('view/backend/adminRegistration.php');
+{
+    //inscription qu'on laissera à disposition ou pas selon le client
+    require 'view/backend/adminRegistration.php';
 }
 function checkRegistration($pseudo, $email, $email2, $password, $password2)
 {
+    //Vérification de pseudo, email et password
     $registrationManager = new \structure\RegistrationManager();
     $registration = $registrationManager->register($pseudo, $email, $email2, $password, $password2);
 
-    if($email == $email2 && $password == $password2) {
-      echo'inscription réussie';
-      require('view/backend/adminLogin.php'); 
-    }else {
-      throw new exception('Merci de revoir vos mots de passe ou emails');
-    }             
+    if ($email == $email2 && $password == $password2) {
+        echo 'inscription réussie';
+        require 'view/backend/adminLogin.php';
+    } else {
+        throw new exception('Merci de revoir vos mots de passe ou emails');
+    }
 }
 function adminLogin()
-{   
-    if(isset($_SESSION['pseudo']) && isset($_SESSION['password'])) {
-      require('view/backend/adminIndex.php');  
+{
+    //On utilise ici le système des sessions pour pour accéder à la page de login
+    if (isset($_SESSION['pseudo']) && isset($_SESSION['password'])) {
+        require 'view/backend/adminIndex.php';
     } else {
-      require('view/backend/adminLogin.php');  
-    }  
+        require 'view/backend/adminLogin.php';
+    }
 }
 
-function checkUser($email,$password)
+function checkUser($email, $password)
 {
+//On controlera ici qu'on a le bon password et que tous les champs sont remplis
     $loginManager = new LoginManager();
-    $member = $loginManager->login($email,$password);
-    $isPasswordCorrect = password_verify($_POST['password'], $member['password']); 
+    $member = $loginManager->login($email, $password);
+    $isPasswordCorrect = password_verify($_POST['password'], $member['password']);
 
-    if(!$member) {  
-      echo 'Mauvais identifiant ou mot de passe';
-      require('view/backend/adminLogin.php'); 
+    if (!$member) {
+        echo 'Mauvais identifiant ou mot de passe';
+        require 'view/backend/adminLogin.php';
     } else {
-          if($isPasswordCorrect) { 
-          $_SESSION['pseudo'] = $member['pseudo'];     
-          $_SESSION['email'] = $member['email'];       
-          getSignaledComments();      
-          } else {
-              echo 'Mauvais identifiant ou mot de passe !'; 
-              require('view/backend/adminLogin.php');            
-              }     
+        if ($isPasswordCorrect) {
+            $_SESSION['pseudo'] = $member['pseudo'];
+            $_SESSION['email'] = $member['email'];
+            getSignaledComments();
+        } else {
+            echo 'Mauvais identifiant ou mot de passe !';
+            require 'view/backend/adminLogin.php';
+        }
     }
 }
 function getSignaledComments()
 {
     $commentManager = new \Structure\CommentManager();
     $signaledComments = $commentManager->getSignaledComments();
-   
+
     if ($signaledComments === false) {
         throw new Exception('aucun commentaire signalé');
     } else {
-      require('view/backend/adminIndex.php');
+        require 'view/backend/adminIndex.php';
     }
 }
 
 function displaySignaledComment()
 {
-  $commentManager = new \Structure\CommentManager();
-  $signaledComments = $commentManager->getSignaledComments();
- 
-  if ($signaledComments === false) {
-      throw new Exception('aucun commentaire signalé');
-  } else {
-    require('view/backend/adminIndexComment.php');
-  }
+    $commentManager = new \Structure\CommentManager();
+    $signaledComments = $commentManager->getSignaledComments();
+
+    if ($signaledComments === false) {
+        throw new Exception('aucun commentaire signalé');
+    } else {
+        require 'view/backend/adminIndexComment.php';
+    }
 }
 function eraseComment($idComment)
 {
     $commentManager = new CommentManager();
     $destroyComment = $commentManager->deleteComment($idComment);
 
-    if($destroyComment === false) {
-      throw new Exception('commentaire non supprimé');
-    }
-    else {
-      require('view/backend/eraseComment.php');
+    if ($destroyComment === false) {
+        throw new Exception('commentaire non supprimé');
+    } else {
+        require 'view/backend/eraseComment.php';
     }
 }
 function saveComment($commentId)
@@ -89,43 +95,43 @@ function saveComment($commentId)
     $commentManager = new CommentManager();
     $updateSignaledComment = $commentManager->retainComment($commentId);
 
-    if($updateSignaledComment === false) {
-      throw new Exception('commentaire non sauvegardé');
-    }
-    else {
-      require('view/backend/saveComment.php');
+    if ($updateSignaledComment === false) {
+        throw new Exception('commentaire non sauvegardé');
+    } else {
+        require 'view/backend/saveComment.php';
     }
 }
 function postListing()
 {
+  //Sur cette vue on ne fera pas de pagination en faveur d'un scrollup
     $postManager = new PostManager();
     $nombredElementsTotal = $postManager->getAllPosts();
-    require('view/backend/adminCrud.php');
+    require 'view/backend/adminCrud.php';
 }
 
 function erasePost($idComment, $idPost)
 {
+//Ici on peut supprimer un post ainsi que le commentaire qui lui est associé
     $postManager = new PostManager();
     $destroyPost = $postManager->deletePost($idComment, $idPost);
 
-    if($destroyPost === false) {
-      throw new Exception('post non supprimé');
-    }
-    else {
-      require('view/backend/erasePost.php');
+    if ($destroyPost === false) {
+        throw new Exception('post non supprimé');
+    } else {
+        require 'view/backend/erasePost.php';
     }
 }
 
 function eraseJustPost($idPost)
 {
+    //Ici on ne supprime que le post
     $postManager = new PostManager();
     $destroyJustPost = $postManager->deleteJustPost($idPost);
 
-    if($destroyJustPost === false) {
-      throw new Exception('Justpost non supprimé');
-    }
-    else {
-      require('view/backend/erasePost.php');
+    if ($destroyJustPost === false) {
+        throw new Exception('Justpost non supprimé');
+    } else {
+        require 'view/backend/erasePost.php';
     }
 }
 
@@ -134,23 +140,21 @@ function readPost($id)
     $postManager = new PostManager();
     $displayPost = $postManager->getPost($id);
 
-    if($displayPost === false) {
-      throw new Exception('post non affiché');
-    }
-    else {
-      require('view/backend/readPost.php');
+    if ($displayPost === false) {
+        throw new Exception('post non affiché');
+    } else {
+        require 'view/backend/readPost.php';
     }
 }
-function updatePost($id, $artist,$title,$albumName,$imageAlbum)
+function updatePost($id, $artist, $title, $albumName, $imageAlbum)
 {
-    $postManager =  new PostManager();
-    $postModified = $postManager->updatePost($id, $artist,$title,$albumName,$imageAlbum);
+    $postManager = new PostManager();
+    $postModified = $postManager->updatePost($id, $artist, $title, $albumName, $imageAlbum);
 
-    if($postModified === false) {
-      throw new Exception('post non modifié');
-    }
-    else {
-      require('view/backend/updateInfo.php');
+    if ($postModified === false) {
+        throw new Exception('post non modifié');
+    } else {
+        require 'view/backend/updateInfo.php';
     }
 }
 function editPost($id)
@@ -158,33 +162,32 @@ function editPost($id)
     $postManager = new PostManager();
     $post = $postManager->getPost($id);
 
-    if($post === false) {
-      throw new exception('post inexistant');
-    }
-    else{
-      require('view/backend/editPost.php');
+    if ($post === false) {
+        throw new exception('post inexistant');
+    } else {
+        require 'view/backend/editPost.php';
     }
 }
 function createPost()
 {
     $postManager = new PostManager();
-    $postAdded = $postManager->addPost();   
+    $postAdded = $postManager->addPost();
 
     if ($postAdded === false) {
         throw new Exception('impossible d\ajouter le post!');
     } else {
-      require('view/backend/createPost.php'); 
-    }    
+        require 'view/backend/createPost.php';
+    }
 }
-function sendPost($artist,$title,$albumName, $imageAlbum)
+function sendPost($artist, $title, $albumName, $imageAlbum)
 {
     $postManager = new PostManager();
     $forward = $postManager->postSender($artist, $title, $albumName, $imageAlbum);
 
     if ($forward === false) {
-      throw new Exception('impossible d\envoyer le post!');
+        throw new Exception('impossible d\envoyer le post!');
     } else {
-      require('view/backend/updateInfo.php');
+        require 'view/backend/updateInfo.php';
     }
 }
 function adminLogout()
@@ -192,7 +195,5 @@ function adminLogout()
     $loginManager = new LoginManager();
     $disconnect = $loginManager->logout();
 
-    header('Location: index.php?action=adminLogout'); 
+    header('Location: index.php?action=adminLogout');
 }
-
-
